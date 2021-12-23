d50d8711ff627738b59573b4443796ff0c590492
Desislava Nikolova <desi011@abv.bg>
2021-12-23 14:58:49 +0200

    Add and update tables and add crud operations

---
+++ database/migrations/2021_12_23_121611_create_product_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('product_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+    }
+}
